2dee61e1c36b200da58ed39c40b46ac97c713ce7
Scott <scrosehart@gmail.com>
2019-01-16 22:51:06 -0800

    fixed super route from post to get and put back on manager file

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // dummy route to add super user
-app.post('/super', async (req, res) => {
+app.get('/super', async (req, res) => {
   const superInfo = {
     firstName: 'super',
     lastName: 'user',
