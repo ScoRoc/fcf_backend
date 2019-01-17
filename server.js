571910e3ee7fee02e3394da61c040b15169b4156
Scott <scrosehart@gmail.com>
2019-01-16 22:14:35 -0800

    heroku fixes

--- server.js
+++ server.js
@@ -34,10 +34,10 @@ app.use('/manager', manager);
 app.use('/wod', wod);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/public')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
