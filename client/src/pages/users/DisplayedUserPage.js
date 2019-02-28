464d0072454d9cd05880ef13f1d178e47f45e629
Scott <scrosehart@gmail.com>
2019-02-27 16:08:38 -0800

    fixed putWithAxios to use correct route

--- client/src/pages/users/DisplayedUserPage.js
+++ client/src/pages/users/DisplayedUserPage.js
@@ -12,18 +12,35 @@ export default class DisplayedUserPage extends React.Component {
     this.state = {
        displayedUser: {},
     }
+    this.newPassword = React.createRef();
+  }
+
+  handleClick = () => {
+    const path = '/user/password';
+    const { putWithAxios } = useAxios(path);
+    putWithAxios({ id: this.state.displayedUser._id, password: this.newPassword }).then(result => {
+      console.log('result.data: ', result.data);
+    });
   }
 
   componentDidMount() {
-    this.setState({ displayedUser: this.props.displayedUser });
+    this.setState({ displayedUser: this.props.displayedUser.user });
   }
 
   render() {
+    const firstName = this.state.displayedUser ? this.state.displayedUser.firstName : 'null';
+    const lastName = this.state.displayedUser ? this.state.displayedUser.lastName : 'null';
+    const email = this.state.displayedUser ? this.state.displayedUser.email : 'null';
     return (
       <section>
         <Link to='/users'>Back</Link>
-        <p>hey from displayed user page</p>
-        <p>{this.state.displayedUser.firstName || 'no name yet'}</p>
+        <p>Name: {firstName} {lastName}</p>
+        <p>Email: {email}</p>
+        <p>Change password:</p>
+        <div>
+          <input ref={this.newPassword} type='text' />
+          <button onClick={this.handleClick}>Submit</button>
+        </div>
       </section>
     );
   }
